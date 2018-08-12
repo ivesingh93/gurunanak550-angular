c5fe5e8535b829a8ed0d30b132ccce9a314f9699
Ivkaran S. Sawhney <ivesingh@gmail.com>
2018-08-12 14:38:15 -0400

    Better structure for the app

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {BodyModule} from "./body/body.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    BodyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/body/body.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import {BodyComponent} from "./body.component";
+import {GalleryComponent} from "./gallery/gallery.component";
+
+@NgModule({
+  declarations: [BodyComponent, GalleryComponent],
+  exports: [
+    BodyComponent
+  ]
+})
+
+export class BodyModule { }
